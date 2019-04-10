de5411203ae9b25d2e65dd4b611259cd94d23677
matthew-collins1991 <matthew.collins1991@yahoo.com>
2019-04-10 17:20:23 +0100

    altered routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
         resources :crawls
         resources :pubs
         get    '/home',  to: 'crawls#index'
-        post '/crawls/new', to: 'crawls#create' 
+        get    '/',  to: 'crawls#index'
+        post '/crawls/new', to: 'crawls#create'
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
