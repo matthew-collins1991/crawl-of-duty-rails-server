d790966747b00563f330ae08ac7cc076b1ef38e2
matthew-collins1991 <matthew.collins1991@yahoo.com>
2019-04-10 16:43:40 +0100

    added some routing

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
         resources :users
         resources :crawls
         resources :pubs
+        get    '/home',  to: 'crawls#index'
+        post '/crawls/new', to: 'crawls#create' 
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
