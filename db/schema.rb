5687fb65ceab88ed4e366dbaba7844b7d2bb453b
matthew-collins1991 <matthew.collins1991@yahoo.com>
2019-04-10 16:16:01 +0100

    prepared for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_03_27_102825) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "crawls", force: :cascade do |t|
     t.string "name"
     t.datetime "start_time"
